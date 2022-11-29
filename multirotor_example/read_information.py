4f37cd2aff4950afaf2ea8a5fc5f2e63b5657f23
j-wye <114757184+j-wye@users.noreply.github.com>
2022-11-29 11:43:57 +0900

    Add files via upload

--- multirotor_example/read_information.py
+++ multirotor_example/read_information.py
@@ -12,20 +12,12 @@ def move(ring):
     data = list(data[0])
     return c.moveToPositionAsync(data[0], data[1], data[2], 7)
 
-while True:
-    # txt파일에서 이름 불러옴
-    with open("drone_ring_list.txt", "r") as f:
-        ring = f.readlines() # ['첫 번째 줄\n', '두 번째 줄\n', '세 번째 줄'] 저장
-        ring = list(map(lambda s: s.strip(), ring))
+with open("drone_ring_list.txt", "r") as f:
+    ring = f.readlines()  # ['첫 번째 줄\n', '두 번째 줄\n', '세 번째 줄'] 저장
+    ring = list(map(lambda s: s.strip(), ring))
 
-    data = list(c.simGetObjectPose(ring[0]))
-    data = list(data[0])
-    c.moveToPositionAsync(data[0], data[1], data[2], 7)
+schedule.every(5).seconds.move(ring)
 
-    for i in range(1,72):
-        data = list(c.simGetObjectPose(ring[i]))
-        data = list(data[0])
-        c.moveToPositionAsync(data[0], data[1], data[2], 5)
 '''
 data = list(c.simGetObjectPose('KDR_Ring_2'))
 data = list(data[0])
